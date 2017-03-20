50caec72676b14cba0bdd7323bbf8fc97cf42270
Ronnie Laugen <ronnie@debian.workgroup>
2017-03-20 22:39:59 +0100

    insert fileinfo to mongodb

--- routes/files.route.ts
+++ routes/files.route.ts
@@ -1,5 +1,7 @@
+import { VisitorCollection } from './../config';
 import express = require('express');
 import multer = require('multer');
+import monk = require('monk');
 
 const upload = multer({
     dest: 'upload/'
@@ -8,7 +10,27 @@ const upload = multer({
 export const router = express.Router({});
 
 router.post('/', upload.single('file'), (req, res) => {
-    console.log(req.file);
+
     console.log(req.body);
-    res.status(200).send({status: 'done'});
+
+    let search = {
+        _id: monk.id(req.body.id)
+    };
+
+    let field = `attachments.${req.body.type}`;
+
+    let update = {
+        $push: {
+            [field]: {
+                filename: req.file.filename,
+                dateSigned: new Date(req.body.date)
+            }
+        }
+    };
+
+    console.log(update);
+
+    VisitorCollection.update(search, update).then(doc => {
+        res.status(200).send(doc);
+    });
 });
