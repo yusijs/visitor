a2208ee4e479c02e640f6f04292c8f2ce98d82ed
Ronnie Laugen <ronnie@debian.workgroup>
2017-03-21 20:47:33 +0100

    Add .pdf to file ext

--- routes/files.route.ts
+++ routes/files.route.ts
@@ -2,16 +2,29 @@ import { VisitorCollection } from './../config';
 import express = require('express');
 import multer = require('multer');
 import monk = require('monk');
+import crypto = require('crypto');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'upload/');
+    },
+    filename: function (req, file, cb) {
+        crypto.pseudoRandomBytes(16, (err, raw) => {
+            if (err) {
+                cb(err, '');
+            }
+            cb(null, raw.toString('hex') + Date.now() + '.pdf');
+        });
+    }
+});
 
 const upload = multer({
-    dest: 'upload/'
+    storage
 });
 
 export const router = express.Router({});
 
 router.post('/', upload.single('file'), (req, res) => {
-
-    console.log(req.body);
 
     let search = {
         _id: monk.id(req.body.id)
@@ -28,8 +41,6 @@ router.post('/', upload.single('file'), (req, res) => {
             }
         }
     };
-
-    console.log(update);
 
     VisitorCollection.update(search, update).then(doc => {
         res.status(200).send(doc);
