f27096ebaaab807a09277b1554f756f47cf46e28
Ronnie Laugen <ronnie@debian.workgroup>
2017-04-09 21:21:16 +0200

    Initial mongoose config

---
+++ config/visitors.schema.ts
@@ -0,0 +1,33 @@
+import * as mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost/visits');
+
+const VisitorSchema = new mongoose.Schema({
+    company: String,
+    name: String,
+    confidentiality: Boolean,
+    attachments: {
+        recording: [{ filename: String, originalName: String, dateSigned: Date }],
+        approved: [{ filename: String, originalName: String, dateSigned: Date }],
+        confidentiality: [{ filename: String, originalName: String, dateSigned: Date }]
+    }
+});
+
+const VisitsSchema = new mongoose.Schema({
+    badge: {
+        badge: Number,
+        noEscort: Boolean,
+        returned: Boolean
+    },
+    comments: String,
+    date: Date,
+    keycard: {
+        keycard: Number,
+        returned: Boolean
+    },
+    site: String,
+    visitor: VisitorSchema
+});
+
+export const VisitModel = mongoose.model('Visit', VisitsSchema);
+export const VisitorModel = mongoose.model('Visitor', VisitorSchema);
